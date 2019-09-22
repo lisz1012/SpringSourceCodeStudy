c091163c4045c47e00715eeabec520f26ccf4aa7
lisz1012 <lisz1012@163.com>
2019-09-21 23:40:22 -0700

    Import Spring for source code review

--- src/main/java/com/lisz/App.java
+++ src/main/java/com/lisz/App.java
@@ -9,8 +9,15 @@ import java.util.stream.Stream;
 import org.junit.Test;
 
 /**
- * Hello world!
- *
+ * 总结：以前框架设计必须考虑：扩展性！！！！
+ * 1.抽象（多态）
+ * 2.设计模式
+ * Spring 给出了扩展
+ * 1.在对象创建之前干点事
+ * 2.在容器初始化之前干点事
+ * 3.在不同的阶段发出不同的事件，你还可以干点事
+ * 4.抽象出各个接口为所欲为
+ * 5.面向接口编程 （类似javax.xxx.xxx, jdbs, service, ejb, jmx, jms, rmi, 全都是接口（借口）找别人实现好甩锅。。。）
  */
 public class App {
 	@Test
@@ -86,16 +93,14 @@ public class App {
 		Field[] fields = clazz.getDeclaredFields();
 		Stream.of(fields).forEach(field -> {
 			if (field.isAnnotationPresent(Autowired.class)) {
-				String name = field.getName();
-				name = "set" + name.substring(0, 1).toUpperCase() + name.substring(1);
+				field.setAccessible(true);
+				Class<?> type = field.getType();
 				try {
-					Method method = clazz.getMethod(name, UserService.class); // 各个参数的类型
-					method.invoke(userController, new UserService());
-				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-					e.printStackTrace();
-				} catch (NoSuchMethodException e) {
-					e.printStackTrace();
-				} catch (SecurityException e) {
+					//Spring会根据配置文件或者注解把这个类型的对象先放在容器Map<String, BeanDefination>中 key就是bean的ID，或者Map<Class, BeanDefination>，HashMap加载因子为0.75
+					//在这里spring并不new而是从这两个map里面拿
+					field.set(userController, type.getConstructor().newInstance()); 
+				} catch (IllegalArgumentException | IllegalAccessException | InstantiationException
+						| InvocationTargetException | NoSuchMethodException | SecurityException e) {
 					e.printStackTrace();
 				}
 			}
